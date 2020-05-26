19f7c8e31498dae56a19d4b676c0a4fe78157e8b
Savo Lazic <savo.lazic@oicr.on.ca>
2020-05-26 14:28:22 -0400

    Load samtools in calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,6 +6,7 @@ set -o pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
-#find all files, return their md5sums to std out
+module load samtools/1.9
+
 for i in *.bam; do  samtools view "$i" | md5sum; done
 cat kmer.txt | md5sum
